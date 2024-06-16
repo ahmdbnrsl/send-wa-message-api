78b0fe7ed9091cd02a23da7642a0accf0ff2df87
abrazax56 <ahmadbenirusli@gmail.com>
2024-06-16 11:38:15 +0700

    disable message listen

--- index.js
+++ index.js
@@ -55,12 +55,12 @@ async function connectionLogic() {
             }
         }
     });
-    sock.ev.on('messages.update', messageInfo => {
+    /*sock.ev.on('messages.update', messageInfo => {
         console.log('receiving message!');
     });
     sock.ev.on('messages.upsert', async ({ messages, type }) => {
         console.log('receiving message!');
-    });
+    });*/
     sock.ev.on('creds.update', saveCreds);
 }
 
